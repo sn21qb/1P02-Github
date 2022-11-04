dbc256e75cb78279879f4a0c3de65db3b96a300a
sn21qb <113047317+sn21qb@users.noreply.github.com>
2022-11-04 16:20:44 -0400

    Update script3.js

--- 1P02 LAB 8/js/script3.js
+++ 1P02 LAB 8/js/script3.js
@@ -3,5 +3,7 @@ document.write("Output Zero Here");
 
 //This will output when a button is pressed
 function theRest(){
-	document.ElementById("output3").innerHTML="Output Three Here";
+	document.getElementById("output1").innerHTML="Output One Here";
+	document.getElementById("output2").innerHTML="Output Two Here";
+	document.getElementById("output3").innerHTML="Output Three Here";
 }
