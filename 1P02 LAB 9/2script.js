cd2dcd93157b9f35cee73bbc1b93a6410851582c
sn21qb <sn21qb@brocku.ca>
2022-11-11 19:55:24 -0500

    updated

--- 1P02 LAB 9/2script.js
+++ 1P02 LAB 9/2script.js
@@ -4,7 +4,7 @@ function promptUp() {
     var fieldOutput = prompt("What is your age?"); //users input
     var todayFullDate = new Date(); //creates a new date
     var year = document.getElementById("num").innerHTML = fieldOutput; //assign users input to var year
-    var todayYear = todayFullDate.getFullYear();
+    var todayYear = todayFullDate.getFullYear(); 
     var birth =  parseInt(todayYear) - parseInt(year)
 
     if (fieldOutput != null) {
